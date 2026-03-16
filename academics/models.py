10d5eb882f69adeb33d0213117d4c796a22bdf75
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-16 21:04:31 +0545

    Created class teacher assignment and subject model

---
+++ academics/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=50)
+    section = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    class_name = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class TeacherAssignment(models.Model):
+    teacher = models.ForeignKey(
+        "accounts.TeacherProfile",
+        on_delete=models.CASCADE
+    )
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    class_name = models.ForeignKey(Class, on_delete=models.CASCADE)
