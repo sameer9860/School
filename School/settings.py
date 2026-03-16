d80824820e4f9b3693691c81fff46bb97a748c8f
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-16 21:03:18 +0545

    Added to installed app

--- School/settings.py
+++ School/settings.py
@@ -44,7 +44,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    
+    'accounts',
+    'core',
+    'academics',
+    'admissions',
+    'notices',
+    'events',
+    'gallery',
+    'results',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.User'
