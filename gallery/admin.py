e0b79ddb965c288bd4322fea3be2ea519c925f59
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-17 20:28:22 +0545

    Added gallery item to admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import GalleryCategory, GalleryImage
+from .models import GalleryCategory, GalleryImage, GalleryItem
 
 admin.site.register(GalleryCategory)
 admin.site.register(GalleryImage)
+admin.site.register(GalleryItem)
