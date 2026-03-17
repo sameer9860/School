0217f9a8fbbc857d20cb1c947e25588521c47c86
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-17 20:26:42 +0545

    Added slide and principle message model

--- core/models.py
+++ core/models.py
@@ -14,3 +14,21 @@ class SchoolInfo(models.Model):
     phone = models.CharField(max_length=20)
     email = models.EmailField()
     about = models.TextField()
+
+
+class Slider(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="slider/")
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PrincipalMessage(models.Model):
+    name = models.CharField(max_length=200)
+    message = models.TextField()
+    photo = models.ImageField(upload_to="principal/")
+
+    def __str__(self):
+        return self.name
