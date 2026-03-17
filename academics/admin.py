aeb10275c922ca1fb09479d6bad7e8d7b5c1d3c0
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-17 20:26:08 +0545

    Added all models to admin

--- academics/admin.py
+++ academics/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Class, Subject, TeacherAssignment
+from .models import Class, Curriculum, Subject, TeacherAssignment
 
 admin.site.register(Class)
 admin.site.register(Subject)
 admin.site.register(TeacherAssignment)
+admin.site.register(Curriculum)
