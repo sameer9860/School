ab4f3ca028b731202ed1f9ed80b188fbffc13057
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-03-17 20:35:39 +0545

    added gallery app

--- School/settings.py
+++ School/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'School.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,5 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'accounts.User'
